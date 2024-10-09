753adfba70c9f16cce034461764823a7cae2a4eb
Jonathan Thomason <razordor@gmail.com>
2024-10-09 14:07:26 -0700

    tested assembler and interpreter

--- tests/mod.rs
+++ tests/mod.rs
@@ -5,9 +5,9 @@ const SRC_DIR: &str = "../stackl-rs/examples/";
 const OUT_DIR: &str = "../stackl-rs/target/";
 
 #[test]
-fn test_assembler() {
-    let mut bin_path: String = BIN_DIR.to_owned();
-    bin_path.push_str("stackl-as");
+fn test_example1() {
+    let mut assembler_path: String = BIN_DIR.to_owned();
+    assembler_path.push_str("stackl-as");
 
     let mut src_path: String = SRC_DIR.to_owned();
     src_path.push_str("test.asm");
@@ -15,11 +15,29 @@ fn test_assembler() {
     let mut out_path: String = OUT_DIR.to_owned();
     out_path.push_str("test.stackl");
 
-    let output = Command::new(bin_path)
+    let mut interp_path: String = BIN_DIR.to_owned();
+    interp_path.push_str("stackl");
+
+    let output = Command::new(assembler_path)
         .arg(src_path)
         .arg("-o")
-        .arg(out_path)
+        .arg(&out_path)
         .output()
-        .expect("Failed to execute command");
+        .expect("Failed to execute 'stackl-as'");
     assert!(output.status.success());
+
+    let output = Command::new(interp_path)
+        .arg(&out_path)
+        .output()
+        .expect("Failed to execute 'stackl'");
+    assert!(output.status.success());
+
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    let mut expected = String::new();
+    for _ in 0..10 {
+        expected.push_str("hello world!\n");
+    }
+
+    assert_eq!(expected, stdout);
 }
