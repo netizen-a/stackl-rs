34275206efb3369a202f58e90931fa05cdbacd69
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-05-30 18:39:45 -0700

    added directive names

--- src/bin/stackl-cc/lex/mod.rs
+++ src/bin/stackl-cc/lex/mod.rs
@@ -5,3 +5,19 @@ pub use pp_token_iter::*;
 
 use lalrpop_util::lalrpop_mod;
 lalrpop_mod!(pub grammar, "/bin/stackl-cc/lex/grammar.rs");
+
+#[macro_export]
+macro_rules! directive {
+	($kind:expr , $name:literal , $lo:ident, $hi:ident) => {
+		if matches!($kind, PPTokenKind::Ident(Ident{name, ..}) if name == $name) {
+			Ok(())
+		} else {
+			Err(lalr::ParseError::User {
+				error: diag::Error{
+					kind: diag::ErrorKind::InvalidToken,
+					loc: ($lo, $hi)
+				}
+			})
+		}
+	}
+}
