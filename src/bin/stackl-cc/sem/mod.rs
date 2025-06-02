bea6485dc2ad2d190c533fe8a087af3a47572b55
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-06-01 21:06:52 -0700

    added placeholders

--- src/bin/stackl-cc/sem/mod.rs
+++ src/bin/stackl-cc/sem/mod.rs
@@ -2,7 +2,7 @@ mod decl;
 mod expr;
 mod stmt;
 
-use crate::syn::TranslationUnit;
+use crate::syn;
 
 pub struct SemanticParser {}
 
@@ -10,7 +10,20 @@ impl SemanticParser {
 	pub fn new() -> Self {
 		Self {}
 	}
-	pub fn parse(&mut self, _unit: TranslationUnit) {
+	pub fn parse(&mut self, unit: Vec<syn::ExternalDeclaration>) {
+		for external_decl in unit {
+			match external_decl {
+				syn::ExternalDeclaration::FunctionDefinition(decl) => {
+					self.function_definition(decl)
+				}
+				syn::ExternalDeclaration::Declaration(decl) => self.declaration(decl),
+			}
+		}
+	}
+	fn function_definition(&mut self, _decl: syn::FunctionDefinition) {
+		todo!()
+	}
+	fn declaration(&mut self, _decl: syn::Declaration) {
 		todo!()
 	}
 }
