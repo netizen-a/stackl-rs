d50815ec6fb49e0e8c1d96083b181edea85e27cf
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-06-02 01:52:09 -0700

    added some semantic functions

--- src/bin/stackl-cc/sem/decl.rs
+++ src/bin/stackl-cc/sem/decl.rs
@@ -1,13 +1,28 @@
 use crate::syn::*;
 
 impl super::SemanticParser {
-	pub(super) fn declaration_specifier(&mut self, _specifier: DeclarationSpecifier) {
-		todo!("declaration-secifier")
+	pub(super) fn declaration_specifier(&mut self, specifier: DeclarationSpecifier) {
+		use DeclarationSpecifier::*;
+		match specifier {
+			StorageClassSpecifier(spec) => self.storage_class_specifier(spec),
+			TypeSpecifier(spec) => self.type_specifier(spec),
+			TypeQualifier(spec) => self.type_qualifier(spec),
+			FunctionSpecifier(spec) => self.function_specifier(spec),
+		}
+	}
+	pub(super) fn storage_class_specifier(&mut self, _spec: StorageClassSpecifier) {
+		todo!("storage-class-specifier")
+	}
+	pub(super) fn type_specifier(&mut self, _spec: TypeSpecifier) {
+		todo!("type-specifier")
+	}
+	pub(super) fn function_specifier(&mut self, _spec: FunctionSpecifier) {
+		todo!("function-specifier")
 	}
 	pub(super) fn declarator(&mut self, _declarator: Declarator) {
 		todo!("declarator")
 	}
-	pub(super) fn compound_stmt(&mut self, _compound_stmt: CompoundStmt) {
-		todo!("compound-statment")
+	pub(super) fn type_qualifier(&mut self, _qual: TypeQualifier) {
+		todo!("type-qualifier")
 	}
 }
---
+++ src/bin/stackl-cc/sem/stmt.rs
@@ -0,0 +1,7 @@
+use crate::syn::*;
+
+impl super::SemanticParser {
+	pub(super) fn compound_stmt(&mut self, _compound_stmt: CompoundStmt) {
+		todo!("compound-statment")
+	}
+}
