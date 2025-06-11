255e27dd147bfb956b3d9bb75d14fef4ebd77964
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-06-11 01:19:24 -0700

    added basic dep graph

--- src/bin/stackl-cc/ir/mod.rs
+++ src/bin/stackl-cc/ir/mod.rs
@@ -1,13 +1,33 @@
-use stackl::dr::Module;
-use stackl::sr::DataType;
+use std::collections::HashMap;
 
-pub struct ModuleBuilder {}
+use stackl::dr::Module;
+use stackl::sr;
+
+pub struct ModuleBuilder {
+	// dependency graph
+	dep: HashMap<u32, Vec<u32>>,
+}
+
 impl ModuleBuilder {
 	pub fn new() -> Self {
-		Self {}
+		Self {
+			dep: HashMap::new(),
+		}
 	}
-	pub fn alloc_var(&mut self, data: DataType) -> u32 {
-		todo!()
+	pub fn push(&mut self) -> u32 {
+		let new_id = self.dep.len() as u32;
+		self.dep.insert(new_id, vec![]);
+		new_id
+	}
+	pub fn i32_add(&mut self, id: u32) -> u32 {
+		let new_id = self.dep.len() as u32;
+		self.dep.insert(new_id, vec![id]);
+		new_id
+	}
+	pub fn i32_mul(&mut self, id: u32) -> u32 {
+		let new_id = self.dep.len() as u32;
+		self.dep.insert(new_id, vec![id]);
+		new_id
 	}
 	pub fn build(self) -> Module {
 		Module {}
