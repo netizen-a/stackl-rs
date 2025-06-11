4f42afe9881fdbd43097207bb1106f883f851861
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-06-11 15:58:28 -0700

    optimized dependency graph

--- src/bin/stackl-cc/ir/mod.rs
+++ src/bin/stackl-cc/ir/mod.rs
@@ -1,32 +1,28 @@
-use std::collections::HashMap;
-
 use stackl::dr::Module;
 use stackl::sr;
 
 pub struct ModuleBuilder {
 	// dependency graph
-	dep: HashMap<u32, Vec<u32>>,
+	dep: Vec<Vec<u32>>,
 }
 
 impl ModuleBuilder {
 	pub fn new() -> Self {
-		Self {
-			dep: HashMap::new(),
-		}
+		Self { dep: Vec::new() }
 	}
 	pub fn push(&mut self) -> u32 {
 		let new_id = self.dep.len() as u32;
-		self.dep.insert(new_id, vec![]);
+		self.dep.push(vec![]);
 		new_id
 	}
 	pub fn i32_add(&mut self, id: u32) -> u32 {
 		let new_id = self.dep.len() as u32;
-		self.dep.insert(new_id, vec![id]);
+		self.dep.push(vec![id]);
 		new_id
 	}
 	pub fn i32_mul(&mut self, id: u32) -> u32 {
 		let new_id = self.dep.len() as u32;
-		self.dep.insert(new_id, vec![id]);
+		self.dep.push(vec![id]);
 		new_id
 	}
 	pub fn build(self) -> Module {
