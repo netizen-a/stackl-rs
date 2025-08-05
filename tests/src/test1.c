4c1a6832d84da91539bfbd16a37c1cd3b5fb48bd
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-08-05 16:02:23 -0700

    added extended asm syntax

--- tests/src/test1.c
+++ tests/src/test1.c
@@ -1,7 +1,8 @@
 // Test main and OUTS
 
 int main() {
-    //asm("OUTS" "" :: "g" ("Hello world\n"));
-    char * s = "hello" "world";
+    label: thing:
+    asm ("OUTS" :: "g" ("Hello world\n"));
+    //char * s = "hello" "world";
     return 0;
 }
