164f8fd77f1fae34e06b39c1916b48b286e9ecce
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-11-15 14:33:17 -0800

    added initializer branch

--- src/bin/stackl-cc/synthesis/icg/decl.rs
+++ src/bin/stackl-cc/synthesis/icg/decl.rs
@@ -12,9 +12,21 @@ impl super::SSACodeGen<'_> {
 		let storage_class = decl.specifiers.storage.as_ref().unwrap();
 		let mut var_id_list = vec![];
 		for init_decl in &decl.init_declarator_list {
-			let var_id = self.builder.variable(type_id, storage_class.clone(), None);
+			let init_id = init_decl
+				.initializer
+				.as_ref()
+				.and_then(|i| self.initializer(i));
+			let var_id = self
+				.builder
+				.variable(type_id, storage_class.clone(), init_id);
 			var_id_list.push(var_id);
 		}
 		Ok(var_id_list.into_boxed_slice())
+	}
+	fn initializer(&mut self, initializer: &syn::Initializer) -> Option<u32> {
+		match initializer {
+			syn::Initializer::Expr(_) => None,
+			syn::Initializer::InitializerList(_, _) => None,
+		}
 	}
 }
