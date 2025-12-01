a8eb42f93ab53de9eba6bc035da48df2f3a7e1c3
Jonathan Thomason <jonathan.a.thomason@gmail.com>
2025-11-30 22:26:43 -0800

    added floating constant to codegen

--- src/bin/stackl-cc/synthesis/icg/expr.rs
+++ src/bin/stackl-cc/synthesis/icg/expr.rs
@@ -3,13 +3,17 @@
 use crate::{
 	analysis::syn::{
 		self,
+		CharKind,
+		FloatingKind,
 		IntegerKind,
 	},
 	synthesis::icg::{
 		DataLayout,
+		FloatLayout,
 		IntegerLayout,
 	},
 };
+use std::mem;
 
 impl super::SSACodeGen<'_> {
 	pub(super) fn expr(&mut self, expr: &syn::Expr) -> u32 {
@@ -20,22 +24,34 @@ impl super::SSACodeGen<'_> {
 	}
 	pub(super) fn constant(&mut self, constant: &syn::Constant) -> u32 {
 		match &constant.kind {
-			syn::ConstantKind::Integer(IntegerKind::I32(num)) => {
-				let layout = &DataLayout::Integer(IntegerLayout {
-					width: 32,
-					is_signed: true,
-				});
-				let result_type = self.resolve_type(layout);
-				self.builder.constant_bit32(result_type, *num as u32);
-				todo!()
-			}
-			syn::ConstantKind::Integer(IntegerKind::U32(num)) => {
+			&syn::ConstantKind::Integer(IntegerKind::U32(num)) => {
 				let layout = &DataLayout::Integer(IntegerLayout {
 					width: 32,
 					is_signed: false,
 				});
 				let result_type = self.resolve_type(layout);
-				self.builder.constant_bit32(result_type, *num as u32);
+				self.builder.constant_bit32(result_type, num);
+				todo!()
+			}
+			&syn::ConstantKind::Integer(IntegerKind::I32(num)) => {
+				let layout = &DataLayout::Integer(IntegerLayout {
+					width: 32,
+					is_signed: true,
+				});
+				let result_type = self.resolve_type(layout);
+				unsafe {
+					self.builder
+						.constant_bit32(result_type, mem::transmute(num));
+				}
+				todo!()
+			}
+			&syn::ConstantKind::Floating(FloatingKind::Float(num)) => {
+				let layout = &DataLayout::Float(FloatLayout { width: 32 });
+				let result_type = self.resolve_type(layout);
+				unsafe {
+					self.builder
+						.constant_bit32(result_type, mem::transmute(num));
+				}
 				todo!()
 			}
 			other => todo!("{other:?}"),
