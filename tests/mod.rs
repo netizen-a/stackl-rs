1ad16c307f798329ed7897321ca28e2bfd40c902
Jonathan A. Thomason <jonathan.a.thomason@gmail.com>
2026-01-31 12:26:27 -0800

    updated test suite

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,31 +2,20 @@
 use std::{
 	env,
 	path::PathBuf,
-	process::{
-		self,
-		ExitCode,
-	},
+	process::Command,
 };
 
-fn to_exitcode(cond: bool) -> ExitCode {
-	if cond {
-		ExitCode::SUCCESS
-	} else {
-		ExitCode::FAILURE
-	}
-}
-
 #[test]
-fn syntax() -> ExitCode {
+fn structs() {
 	let compiler_path = PathBuf::from(env!("CARGO_BIN_EXE_stackl-cc"));
-	let test1_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/src/syntax.c");
+	let test1_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/src/structs.c");
 	println!("path: {}", test1_path.display());
-	let out = process::Command::new(compiler_path)
+	let out = Command::new(compiler_path)
 		.arg(test1_path)
 		.arg("--trace")
 		.output()
 		.unwrap();
 	println!("stdout:\n{}", String::from_utf8(out.stdout).unwrap());
 	println!("stderr:\n{}", String::from_utf8(out.stderr).unwrap());
-	to_exitcode(out.status.success())
+	assert!(out.status.success())
 }
